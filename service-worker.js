fa695d36cf4878647af26da9fa26f099ca5d2ddf
relaytalk <lusterchat@gmail.com>
2026-01-01 16:48:24 +0530

    Unable to fix all bugs 🐛  For Now Version One is good, after some time I will move again on fixing bugs,it's the perfect version. you can see.    RelayTalk,
    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for RelayTalk - Caches Everything
-const CACHE_NAME = 'relaytalk-cache-v15';
+const CACHE_NAME = 'relaytalk-cache-v16';
 const APP_VERSION = '1.0.0';
 
 // Cache ALL files from these folders
