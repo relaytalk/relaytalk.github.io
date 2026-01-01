cc495cff797b198737386c892148402f695ff5ae
relaytalk <lusterchat@gmail.com>
2026-01-01 09:49:58 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for RelayTalk - Caches Everything
-const CACHE_NAME = 'relaytalk-cache-v4';
+const CACHE_NAME = 'relaytalk-cache-v5';
 const APP_VERSION = '1.0.0';
 
 // Cache ALL files from these folders
